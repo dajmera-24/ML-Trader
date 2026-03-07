59fd13e8e090f94f5ce6a0e3129ca0ddb1ed0411
Dhruv Ajmera <dhruvajmera0@gmail.com>
2026-03-07 15:22:11 -0600

    completed data stratification

--- main.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-import pandas as pd
-import sys
-import csv
-
-if input('This is the GUI-less interface. Type \'Y\' if you would like to proceed, and N if you would like to exit.') == 'N':
-    sys.exit()
-
-tickers = input('Please enter the tickers you would like to look into, comma separated >>> ').split(',')
